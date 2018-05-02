bce2247dbf2835618cad171a3dff197c25b9f154
SiyuXiu <siyuxiu@gmail.com>
2018-05-02 00:21:45 -0500

    update
    add _exit

--- sxx4663_pfinal.s
+++ sxx4663_pfinal.s
@@ -63,8 +63,19 @@ LDR R0, =printf_str     @ R0 contains formatted string address
 BL printf               @ call printf
 POP {PC}                @ restore the stack pointer and return
 
+_exit:
+MOV R7, #4              @ write syscall, 4
+MOV R0, #1              @ output stream to monitor, 1
+MOV R2, #21             @ print string length
+LDR R1, =exit_str       @ string at label exit_str:
+SWI 0                   @ execute syscall
+MOV R7, #1              @ terminate syscall, 1
+SWI 0                   @ execute syscall
+
+
 .data
 a:              .skip       40
 format_str:     .asciz      "%d"
 start_message:             .asciz      "Enter 10 positive integers, each followed by ENTER:\n"
 printf_str:     .asciz      "a[%d] = %d\n"
+exit_str:       .ascii      "Terminating program.\n"
